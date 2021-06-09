75da4dacdec45acf1270371c7a3aedf4dc43d846
antonbaloo <flomaIZR@gmail.com>
2021-06-09 23:32:59 +0300

    n

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
 //here will by JavaScript code ...
+
+//console.log('console.log');
+
+
+var a = false;
+var b = true;
+console.log('a = false b = true');
+
+
+console.log('a && b', a && b);
+console.log('!a && b', !a && b);
+console.log('!a && !b', !a && !b);
+
+console.log('a || b', a || b);
+console.log('!a || b', !a || b);
+console.log('a || !b', a || !b);
