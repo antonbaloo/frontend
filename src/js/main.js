52a2e3b9819b7336a3e95a84d43523de1227e4ff
antonbaloo <flomaIZR@gmail.com>
2021-06-19 16:18:28 +0300

    wth anatoli grid

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,1 @@
 //here will by JavaScript code ...
-
-//console.log('console.log');
-
-
-var a = false;
-var b = true;
-console.log('a = false b = true');
-
-
-console.log('a && b', a && b);
-console.log('!a && b', !a && b);
-console.log('!a && !b', !a && !b);
-
-console.log('a || b', a || b);
-console.log('!a || b', !a || b);
-console.log('a || !b', a || !b);
